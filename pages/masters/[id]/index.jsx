854ef8cf708b21d87c5460c411994162d5a43861
soulflamecompany <soulflamemusic@gmail.com>
2022-12-19 17:35:12 +0300

    full project

---
+++ pages/masters/[id]/index.jsx
@@ -0,0 +1,10 @@
+import styles from "../../../../pages/masters/Masters.module.scss";
+
+const Master = () => {
+  return;
+  <div className={styles.container}>
+    <div className={styles.contentBackground}>1</div>
+  </div>;
+};
+
+export default Master;
